02906355a4c323b204dfd62ba7c71e7657aeb934
Joey Padasian <joey.padasian@jellyfish.co.uk>
2018-04-23 19:19:59 +0100

    chore: add gh-pages deploy method

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  pathPrefix: 'gatsby-starter-portfolio-emma', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   siteTitle: 'Emma', // Navigation and Site Title
   siteTitleAlt: 'Emma - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-emma.netlify.com/', // Domain of your site. No trailing slash!
+  siteUrl: 'https://joemidi.github.io/', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
   siteDescription: 'Minimalistic bright portfolio with full-width grid and large images',
